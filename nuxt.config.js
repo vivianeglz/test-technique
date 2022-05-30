fa6708ebe15f227a29e2f880b2a0202bf135c392
vivianeglz <viviane.gonzalez@outlook.fr>
2022-05-30 14:23:27 +0200

    :tada: (setup) : Add global setup project

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+export default {
+  ssr: false,
+  target: "static",
+  head: {
+    title: "test-technique",
+    htmlAttrs: {
+      lang: "en"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&family=Roboto:wght@400;700&display=swap"
+      }
+    ]
+  },
+  css: ["@/assets/style/main.scss"],
+  plugins: [],
+  components: false,
+  buildModules: ["@nuxtjs/eslint-module", "@nuxtjs/stylelint-module"],
+  modules: ["@nuxtjs/axios"],
+  axios: {
+    baseURL: "/"
+  },
+  build: {}
+};
